428f52df0a0cc9c1690b8ff53db4d17a70ff23a6
Bijan Jadhavji <12b.jad@gmail.com>
2025-11-07 18:08:08 -0600

    updated some things, kev gonna check it out

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-import express from "express";
+const express = require("express");
 const app = express();
-export default app;
+
+app.use(express.json());
+
+const usersRouter = require("./api/users");
+const productRouter = require("./api/products");
+const ordersRouter = require("./api/orders");
+
+app.use("/users", usersRouter);
+app.use("/products", productRouter);
+app.use("/orders", ordersRouter);
+
+module.exports = app;
--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,8 @@
 import db from "#db/client";
+import { createOrder } from "#db/queries/orders";
+import { createOrderProduct } from "#db/queries/ordersProducts";
+import { createProduct } from "#db/queries/products";
+import { createUser } from "#db/queries/users";
 
 await db.connect();
 await seed();
@@ -6,5 +10,21 @@ await db.end();
 console.log("🌱 Database seeded.");
 
 async function seed() {
-  // TODO
+  // Create 1 user
+  const user = await createUser("superstar", "loves2shop");
+
+  // Create 10 products
+  for (let i = 1; i <= 10; i++) {
+    await createProduct(
+      "Product " + i,
+      "Description " + i,
+      Math.random() * 100,
+    );
+  }
+
+  // User makes an order of 5 products
+  const order = await createOrder(user.id, "7777-07-07");
+  for (let i = 1; i <= 5; i++) {
+    await createOrderProduct(order.id, i, i);
+  }
 }
